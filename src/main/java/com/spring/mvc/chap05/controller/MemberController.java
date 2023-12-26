9a5712240c53c3bf9e89b74d4d8f6d17881668e2
yongseopK <kk0@kakao.com>
2023-12-26 10:43:35 +0900

    #25. 쿠키

--- src/main/java/com/spring/mvc/chap05/controller/MemberController.java
+++ src/main/java/com/spring/mvc/chap05/controller/MemberController.java
@@ -8,10 +8,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/members")
@@ -61,7 +62,8 @@ public class MemberController {
             LoginRequestDTO dto,
             // Model에 담긴 데이터는 리다이렉트시 jsp로 가지 않는다.
             // 왜냐면 리다이텍트는 요청이 2번 들어가서 첫번째 요청시 보관한 데이터는 소실됨
-            RedirectAttributes ra
+            RedirectAttributes ra,
+            HttpServletResponse response
     ) {
 
         log.info("/members/sign-in POST !");
@@ -73,9 +75,23 @@ public class MemberController {
         ra.addFlashAttribute("msg", result);
 
         if(result == LoginResult.SUCCESS) {  // 로그인 성공시
-            return "redirect:/board/list";
+
+            makeLoginCookie(dto, response);
+
+            return "redirect:/";
         }
         return "redirect:/members/sign-in"; // 로그인 실패시
 
+    }
+
+    private static void makeLoginCookie(LoginRequestDTO dto, HttpServletResponse response) {
+        // 쿠키에 로그인 기록을 저장
+        Cookie cookie = new Cookie("login", dto.getAccount());
+        // 쿠키 정보 세팅
+        cookie.setPath("/");    // 이 쿠키는 모든 경로에서 들고다녀야 함
+        cookie.setMaxAge(60);   // 쿠키 수명 설정
+
+        // 쿠키를 클라이언트에게 전송 (Response객체 필요)
+        response.addCookie(cookie);
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -5,8 +5,33 @@
 <head>
     <meta charset="UTF-8">
     <title>Web study</title>
+    <%@ include file="include/static-head.jsp"%>
+
+    <style>
+        h1 {
+            margin: 200px auto;
+            width: 40%;
+            font-size: 40px;
+            font-weight: 700;
+            color: orange;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    <h1>안녕하세요~~</h1>
+
+    <%
+        String userName = "방문자";
+        // 클라이언트에게 쿠키를 검사
+        Cookie[] cookies = request.getCookies();
+        for (Cookie c : cookies) {
+            if(c.getName().equals("login")) {
+                userName = c.getValue();
+            }
+        }
+    %>
+
+    <%@ include file="include/header.jsp"%>
+    <h1> <%= userName %>님 안녕하세요~~</h1>
 </body>
 </html>
