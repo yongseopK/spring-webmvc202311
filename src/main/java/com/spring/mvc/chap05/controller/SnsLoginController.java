bb574a84e4fb37bc7953fc27a5240f99b04ef0f2
yongseopK <kk0@kakao.com>
2024-01-04 10:52:35 +0900

    #38. 카카오 로그인 토큰 발급받기

--- src/main/java/com/spring/mvc/chap05/controller/SnsLoginController.java
+++ src/main/java/com/spring/mvc/chap05/controller/SnsLoginController.java
@@ -1,13 +1,19 @@
 package com.spring.mvc.chap05.controller;
 
 
+import com.spring.mvc.chap05.service.SnsLoginService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class SnsLoginController {
 
     @Value("${sns.kakao.app-key}")
@@ -15,6 +21,8 @@ public class SnsLoginController {
 
     @Value("${sns.kakao.redirect-uri}")
     private String kakaoRedirectUri;
+
+    private final SnsLoginService snsLoginService;
 
     // 카카오 인가코드 발급 요청
     @GetMapping("/kakao/login")
@@ -30,6 +38,15 @@ public class SnsLoginController {
     @GetMapping("/auth/kakao")
     public String snsKakao(String code) {
         log.info("카카오로그인 인가코드 : {}", code);
+
+        // 인가 코드를 가지고 카카오 인증서버에 토큰 발급요청을 보냄
+        // server to server 통신
+        Map<String, String> params = new HashMap<>();
+        params.put("appkey", kakaoAppKey);
+        params.put("redirect", kakaoRedirectUri);
+        params.put("code", code);
+        snsLoginService.kakaoLogin(params);
+
         return "redirect:/members/sign-in";
     }
 }
---
+++ src/main/java/com/spring/mvc/chap05/service/SnsLoginService.java
@@ -0,0 +1,70 @@
+package com.spring.mvc.chap05.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+@Slf4j
+public class SnsLoginService {
+
+    // 카카오 로그인 처리
+    public void kakaoLogin(Map<String, String> requestParam) {
+
+        // 인가코드를 가지고 토큰을 발급받는 요청
+        String accessToken = getKakaoAccessToken(requestParam);
+        log.debug("access_token {}", accessToken);
+    }
+
+    // 토큰발급 요청
+    private String getKakaoAccessToken(Map<String, String> requestParam) {
+
+        // 요청 URI
+        String requsetUri = "https://kauth.kakao.com/oauth/token";
+
+        // 요청 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type",
+                    "application/x-www-form-urlencoded;charset=utf-8");
+
+        // 요청 바디에 파라미터 설정
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", requestParam.get("appkey"));
+        params.add("redirect_uri", requestParam.get("redirect"));
+        params.add("code", requestParam.get("code"));
+
+        RestTemplate template = new RestTemplate();
+
+        HttpEntity<Object> requestEntity = new HttpEntity<>(params, headers);
+
+        /*
+            - RestTemplate객체가 REST API 통신을 위한 API인데(js -> fetch)
+            - 서버에 통신을 보내면서 응답을 받을 수 있는 메서드가 exchange임
+
+            param1 : 요청 URL
+            param2 : 요청 방식 (get, post, put, patch, delete...)
+            param3 : 요청 헤더와 요청 바디정보 - HttpEntity로 포장해서 줘야 함
+            param4 : 응답결과(JSON)를 어떤 타입으로 받아낼 것이지 (ex : DTO로 받을건지 Map으로 받을건지)
+         */
+        ResponseEntity<Map> responseEntity = template.exchange(requsetUri, HttpMethod.POST, requestEntity, Map.class);
+
+        // 응답 데이터에서 JSON 추출
+        Map<String, Object> responseJSON = (Map<String, Object>) responseEntity.getBody();
+        log.debug("데이터 : {}", responseJSON);
+
+        // access token 추출
+        return (String) responseJSON.get("access_token");
+
+    }
+}
